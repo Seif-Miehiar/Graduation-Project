02619575e141826c8a692c5068cbcc71bc439950
Seif-Miehiar <Saif.muhiar97@gmail.com>
2019-06-22 13:50:32 +0300

    editing shop page and headers

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,155 @@ class Header extends React.Component {
   render(){
     return(
 <div>
-<div className="banner banner2">
+<header>
+			{/* <!-- TOP HEADER --> */}
+			<div id="top-header">
+				<div className="container">
+					<ul className="header-links pull-left">
+						<li><a href="/"><i className="fa fa-phone"></i> +021-95-51-84</a></li>
+						<li><a href="/"><i className="fa fa-envelope-o"></i> email@email.com</a></li>
+						<li><a href="/"><i className="fa fa-map-marker"></i> 1734 Stonecoal Road</a></li>
+					</ul>
+					<ul className="header-links pull-right">
+						<li><a href="/"><i className="fa fa-dollar"></i> USD</a></li>
+						<li><a href="/"><i className="fa fa-user-o"></i> My Account</a></li>
+					</ul>
+				</div>
+			</div>
+			{/* <!-- /TOP HEADER --> */}
+
+			{/* <!-- MAIN HEADER --> */}
+			<div id="header">
+				{/* <!-- container --> */}
+				<div className="container">
+					{/* <!-- row --> */}
+					<div className="row">
+						{/* <!-- LOGO --> */}
+						<div className="col-md-3">
+							<div className="header-logo">
+								<a href="/" className="logo">
+									<img src="./img/logo.png" alt=""/>
+								</a>
+							</div>
+						</div>
+						{/* <!-- /LOGO --> */}
+
+						{/* <!-- SEARCH BAR --> */}
+						<div className="col-md-6">
+							<div className="header-search">
+								<form>
+									<select className="input-select">
+										<option value="0">All Categories</option>
+										<option value="1">Category 01</option>
+										<option value="1">Category 02</option>
+									</select>
+									<input className="input" placeholder="Search here" />
+									<button className="search-btn">Search</button>
+								</form>
+							</div>
+						</div>
+						{/* <!-- /SEARCH BAR --> */}
+
+						{/* <!-- ACCOUNT --> */}
+						<div className="col-md-3 clearfix">
+							<div className="header-ctn">
+								{/* <!-- Wishlist --> */}
+								<div>
+									<a href="/">
+										<i className="fa fa-heart-o"></i>
+										<span>Your Wishlist</span>
+										<div className="qty">2</div>
+									</a>
+								</div>
+								{/* <!-- /Wishlist --> */}
+
+								{/* <!-- Cart --> */}
+								<div className="dropdown">
+									<a className="dropdown-toggle" href="/" data-toggle="dropdown" aria-expanded="true">
+										<i className="fa fa-shopping-cart"></i>
+										<span>Your Cart</span>
+										<div className="qty">3</div>
+									</a>
+									<div className="cart-dropdown">
+										<div className="cart-list">
+											<div className="product-widget">
+												<div className="product-img">
+													<img src="./img/product01.png" alt=""/>
+												</div>
+												<div className="product-body">
+													<h3 className="product-name"><a href="/">product name goes here</a></h3>
+													<h4 className="product-price"><span className="qty">1x</span>$980.00</h4>
+												</div>
+												<button className="delete"><i className="fa fa-close"></i></button>
+											</div>
+
+											<div className="product-widget">
+												<div className="product-img">
+													<img src="./img/product02.png" alt=""/>
+												</div>
+												<div className="product-body">
+													<h3 className="product-name"><a href="/">product name goes here</a></h3>
+													<h4 className="product-price"><span className="qty">3x</span>$980.00</h4>
+												</div>
+												<button className="delete"><i className="fa fa-close"></i></button>
+											</div>
+										</div>
+										<div className="cart-summary">
+											<small>3 Item(s) selected</small>
+											<h5>SUBTOTAL: $2940.00</h5>
+										</div>
+										<div className="cart-btns">
+											<a href="/">View Cart</a>
+											<a href="/">Checkout  <i className="fa fa-arrow-circle-right"></i></a>
+										</div>
+									</div>
+								</div>
+								{/* <!-- /Cart --> */}
+
+								{/* <!-- Menu Toogle --> */}
+								<div className="menu-toggle">
+									<a href="/">
+										<i className="fa fa-bars"></i>
+										<span>Menu</span>
+									</a>
+								</div>
+								{/* <!-- /Menu Toogle --> */}
+							</div>
+						</div>
+						{/* <!-- /ACCOUNT --> */}
+					</div>
+					{/* <!-- row --> */}
+				</div>
+				{/* <!-- container --> */}
+			</div>
+			{/* <!-- /MAIN HEADER --> */}
+		</header>
+		{/* <!-- /HEADER --></div> */}
+		{/* <!-- NAVIGATION --> */}
+		<nav id="navigation">
+			{/* <!-- container --> */}
+			<div class="container">
+				{/* <!-- responsive-nav --> */}
+				<div id="responsive-nav">
+					{/* <!-- NAV --> */}
+					<ul class="main-nav nav navbar-nav">
+						<li><a href="/">Home</a></li>
+						<li class="active"><a href="/">Hot Deals</a></li>
+						<li><a href="/">Categories</a></li>
+						<li><a href="/">Laptops</a></li>
+						<li><a href="/">Smartphones</a></li>
+						<li><a href="/">Cameras</a></li>
+						<li><a href="/">Accessories</a></li>
+					</ul>
+					{/* <!-- /NAV --> */}
+				</div>
+				{/* <!-- /responsive-nav --> */}
+			</div>
+			{/* <!-- /container --> */}
+		</nav>
+		{/* <!-- /NAVIGATION --> */}
+
+{/* <div className="banner banner2">
 	 <div className="container">
 		 <div className="headr-right">
 				<div className="details">
@@ -36,15 +184,15 @@ class Header extends React.Component {
 			     <div className="content white">
 					 <nav className="navbar navbar-default">
 						 <div className="navbar-header">
-							<button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<button type="button" className="navbar-toggle" data-toggle="collapse" data-target="/bs-example-navbar-collapse-1">
 								<span className="sr-only">Toggle navigation</span>
 								<span className="icon-bar"></span>
 								<span className="icon-bar"></span>
 								<span className="icon-bar"></span>
 							</button>				
-						 </div>
+						 </div> */}
 						 {/* <!--/navbar header-->		 */}
-						 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						 {/* <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							 <ul className="nav navbar-nav">
 								 <li><Link to="/sign-in-customer">Shop</Link></li>
 								 <li><a href="about.html">About</a></li>
@@ -59,18 +207,18 @@ class Header extends React.Component {
 								 <li><a href="gallery.html">Gallery</a></li>
 								 <li><a href="contact.html">Contact</a></li>
 							 </ul>
-							</div>
+							</div> */}
 						  {/* <!--/navbar collapse--> */}
-					 </nav>
+					 {/* </nav> */}
 					  {/* <!--/navbar--> */}
-				 </div>
+				 {/* </div>
 					 <div className="clearfix"></div>
 					<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
 			  </div>
 				 <div className="clearfix"></div>
 		  </div>
 	 </div>	 
-</div>
+</div> */}
 
 </div>
 
--- src/components/shopnew.js
+++ src/components/shopnew.js
@@ -41,8 +41,149 @@ class Shopnew extends React.Component {
 
     return (
       <div>
-        {/* <Header /> */}
+        <Header />
+
+        {/* <!-- SECTION --> */}
+		<div className="section">
+			{/* <!-- container --> */}
+			<div className="container">
+				{/* <!-- row --> */}
+				<div className="row">
+					{/* <!-- shop --> */}
+          {/* {this.state.products.map((oneProd, i) */}
         
+					<div className="col-md-4 col-xs-6">
+						<div className="shop">
+							<div className="shop-img">
+								<img src={"./cssshop/img/shop01.png"} alt=""/>
+							</div>
+							<div className="shop-body">
+								<h3>Laptop<br/>Collection</h3>
+								<a href="/" className="cta-btn">Shop now <i className="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+					{/* <!-- /shop --> */}
+
+					{/* <!-- shop --> */}
+					<div className="col-md-4 col-xs-6">
+						<div className="shop">
+							<div className="shop-img">
+								<img src="./cssshop/img/shop03.png" alt=""/>
+							</div>
+							<div className="shop-body">
+								<h3>Accessories<br/>Collection</h3>
+								<a href="/" className="cta-btn">Shop now <i className="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+					{/* <!-- /shop --> */}
+
+					{/* <!-- shop --> */}
+					<div className="col-md-4 col-xs-6">
+						<div className="shop">
+							<div className="shop-img">
+								<img src="./cssshop/img/shop02.png" alt=""/>
+							</div>
+							<div className="shop-body">
+								<h3>Cameras<br/>Collection</h3>
+								<a href="/" className="cta-btn">Shop now <i className="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+					{/* <!-- /shop --> */}
+				</div>
+				{/* <!-- /row --> */}
+			</div>
+			{/* <!-- /container --> */}
+		</div>
+		{/* <!-- /SECTION --> */}
+
+		{/* <!-- SECTION --> */}
+		<div class="section">
+			{/* <!-- container --> */}
+			<div class="container">
+				{/* <!-- row --> */}
+				<div class="row">
+
+					{/* <!-- section title --> */}
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">New Products</h3>
+							<div class="section-nav">
+								<ul class="section-tab-nav tab-nav">
+									<li class="active"><a data-toggle="tab" href="#tab1">Laptops</a></li>
+									<li><a data-toggle="tab" href="#tab1">Smartphones</a></li>
+									<li><a data-toggle="tab" href="#tab1">Cameras</a></li>
+									<li><a data-toggle="tab" href="#tab1">Accessories</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					{/* <!-- /section title --> */}
+
+					{/* <!-- Products tab & slick --> */}
+					<div class="col-md-12">
+						<div class="row">
+							<div class="products-tabs">
+								{/* <!-- tab --> */}
+								<div id="tab1" class="tab-pane active">
+									<div class="products-slick" data-nav="#slick-nav-1">
+										{/* <!-- product --> */}
+										<div class="product">
+											<div class="product-img">
+												<img src="./cssshop/img/product01.png" alt=""/>
+												<div class="product-label">
+													<span class="sale">-30%</span>
+													<span class="new">NEW</span>
+												</div>
+											</div>
+											<div class="product-body">
+												<p class="product-category">Category</p>
+												<h3 class="product-name"><a href="/">product name goes here</a></h3>
+												<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
+												<div class="product-rating">
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+													<i class="fa fa-star"></i>
+												</div>
+												<div class="product-btns">
+													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+													<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
+													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+												</div>
+											</div>
+											<div class="add-to-cart">
+												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+											</div>
+                      </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div></div>
+                    </div>
+                    </div>
+										{/* <!-- /product --> */}
+
+
+
+
+
+    {this.state.products.map((prodo, i) => {
+            
+            return (
+              <div>
+
+              </div>
+            )
+          })}
+
+
+
+
           <div>
             {this.state.products.map((oneProd, i) => {
               console.log(oneProd.image)
